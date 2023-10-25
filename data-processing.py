89d2cfb4eb6395f58aa75f013f41720d297ef802
Tucker26 <108190126+Tucker26@users.noreply.github.com>
2023-10-25 10:05:11 -0500

    Data Processing Overhaul - Update 2 - Code not functioning

--- data-processing.py
+++ data-processing.py
@@ -46,86 +46,84 @@ df = pd.get_dummies(df,columns=['BTN_TOUCH'],drop_first=True) # drop first to pr
     # don't store: loop through the data, extracting the features from a gesture and storing the feature immediately after gesture is identified
         # does it even matter which one? is this unnecessary optimization?
 
-current_gesture = pd.DataFrame
-i = 0  # count variable for rows in gesture
-
-for row in df:
-    if i >= GESTURE_SIZE:  # todo: include lifting finger as condition?
-        extract_features(current_gesture)
-        current_gesture = pd.DataFrame(columns=current_gesture.columns)  # clear data but keep columns
-        continue
-    current_gesture.append(row)
-    i += 1
-
-
-
-# for rows in csv
-    # while user = 0
-        # for i in range gesture size:
-            # add to gesture (is the gesture one rows of averages? a full separated collection of rows?
-        # add gesture to gesture csv/df
-    # while user = 1
-        # for i in range gesture size
-            # add to gesture
-        # add gesture to gesture csv/df
-
-
 
 # FEATURE EXTRACTION
 # our goal is to output a csv file containing a row of features for each gesture
-df.insert(len(df.columns), "X_Speed", 0)
-df.insert(len(df.columns), "X_Acceleration", 0)
-df.insert(len(df.columns), "Y_Speed", 0)
-df.insert(len(df.columns), "Y_Acceleration", 0)
-df.insert(len(df.columns), "Speed", 0)
-df.insert(len(df.columns), "Acceleration", 0)
-df.insert(len(df.columns), "Jerk", 0)
-df.insert(len(df.columns), "Ang_V", 0)
-df.insert(len(df.columns), "Path_Tangent", 0)
-# possible additional features:
+features = pd.DataFrame()
+
+def extract_features(gesture):
+    gesture.insert(len(gesture.columns), "X_Speed", 0)
+    gesture.insert(len(gesture.columns), "X_Acceleration", 0)
+    gesture.insert(len(gesture.columns), "Y_Speed", 0)
+    gesture.insert(len(gesture.columns), "Y_Acceleration", 0)
+    gesture.insert(len(gesture.columns), "Speed", 0)
+    gesture.insert(len(gesture.columns), "Acceleration", 0)
+    gesture.insert(len(gesture.columns), "Jerk", 0)
+    gesture.insert(len(gesture.columns), "Ang_V", 0)
+    gesture.insert(len(gesture.columns), "Path_Tangent", 0)
+    # possible additional features:
     # todo: does additional features increase/decrease over/underfitting?
     # size/range of gesture: the area of screen touched
-        # distance travelled
-        # distance end to end
-        # average direction: direction end to end
+    # distance travelled
+    # distance end to end
+    # average direction: direction end to end
     # curvature
     # type should each gesture have a single type? should we divide gestures by type? should we include time off screen as a type?
     # smoothness: how smooth the motion is vs jittery/jagged
     # elapsed time
     # additionally we should include min, max, avg, std for features when we can
     # x and y start and end points
-        # is it valuable to break this up and include midpoints
-            #(I dont think so, we could add all sorts of features for midpoints but we are then essentially splitting one feature into 2)
+    # is it valuable to break this up and include midpoints
+    # (I dont think so, we could add all sorts of features for midpoints but we are then essentially splitting one feature into 2)
     # sum of angles: add up all the angles from each data point
     # sharp angles: count how many times the angle change exceeds a given constant
     # time until movement: time taken for a user to move their finger after touching the screen
-# how do we determine which features to use?
+    # how do we determine which features to use?
     # is it possible to test all combinations of features and graph our results? or would that take too long
-# should raw data be considered in features?
+    # should raw data be considered in features?
 
-# I am still unsure if this is how we should be calculating features
-df['X_Speed'] = (df.X - df.X.shift(1)) / (df.Timestamp - df.Timestamp.shift(1))
-df['Y_Speed'] = (df.Y - df.Y.shift(1)) / (df.Timestamp - df.Timestamp.shift(1))
-df['Speed'] = np.sqrt((df.X_Speed ** 2) + (df.Y_Speed ** 2))
-df['X_Acceleration'] = (df.X_Speed - df.X_Speed.shift(1)) / (df.Timestamp - df.Timestamp.shift(1))
-df['Y_Acceleration'] = (df.Y_Speed - df.Y_Speed.shift(1)) / (df.Timestamp - df.Timestamp.shift(1))
-df['Acceleration'] = (df.Speed - df.Speed.shift(1)) / (df.Timestamp - df.Timestamp.shift(1))
-df['Jerk'] = (df.Acceleration - df.Acceleration.shift(1)) / (df.Timestamp - df.Timestamp.shift(1))
-df['Path_Tangent'] = np.arctan2((df.Y - df.Y.shift(1)), (df.X - df.X.shift(1)))
-df['Ang_V'] = (df.Path_Tangent - df.Path_Tangent.shift(1)) / (df.Timestamp - df.Timestamp.shift(1))
+    # I am still unsure if this is how we should be calculating features
+    gesture['X_Speed'] = (gesture.X - gesture.X.shift(1)) / (gesture.Timestamp - gesture.Timestamp.shift(1))
+    gesture['Y_Speed'] = (gesture.Y - gesture.Y.shift(1)) / (gesture.Timestamp - gesture.Timestamp.shift(1))
+    gesture['Speed'] = np.sqrt((gesture.X_Speed ** 2) + (gesture.Y_Speed ** 2))
+    gesture['X_Acceleration'] = (gesture.X_Speed - gesture.X_Speed.shift(1)) / (
+                gesture.Timestamp - gesture.Timestamp.shift(1))
+    gesture['Y_Acceleration'] = (gesture.Y_Speed - gesture.Y_Speed.shift(1)) / (
+                gesture.Timestamp - gesture.Timestamp.shift(1))
+    gesture['Acceleration'] = (gesture.Speed - gesture.Speed.shift(1)) / (
+                gesture.Timestamp - gesture.Timestamp.shift(1))
+    gesture['Jerk'] = (gesture.Acceleration - gesture.Acceleration.shift(1)) / (
+                gesture.Timestamp - gesture.Timestamp.shift(1))
+    gesture['Path_Tangent'] = np.arctan2((gesture.Y - gesture.Y.shift(1)), (gesture.X - gesture.X.shift(1)))
+    gesture['Ang_V'] = (gesture.Path_Tangent - gesture.Path_Tangent.shift(1)) / (
+                gesture.Timestamp - gesture.Timestamp.shift(1))
+
+    return gesture
+
+
+current_gesture = pd.DataFrame(columns=df.columns)
+i = 0  # count variable for rows in gesture
+
+for row in df:
+    if i >= GESTURE_SIZE:  # todo: include lifting finger as condition? and switching fingers
+        features.loc[len(features.index)] = extract_features(current_gesture) # append extracted features to features
+        current_gesture = pd.DataFrame(columns=current_gesture.columns)  # clear data but keep columns
+        continue
+    current_gesture.loc[len(current_gesture.index)] = row  # append row to current_gesture
+    i += 1
+
 
 # Drop missing or NaN values
-df.dropna(inplace=True)
-
+features = features.dropna(inplace=True)
 # scaling
 scaler = StandardScaler()
-scaler.fit(df)
-df = pd.DataFrame(scaler.transform(df),columns=df.keys())
+scaler.fit(features)
+features = pd.DataFrame(scaler.transform(features),columns=features.keys())
 
 # # normalization
 # for column in df.columns:
 #     df[column] = df[column] / df[column].abs().max()
 
 # SAVE TO CSV
-df['User'] = user
-df.to_csv('processed-feature-data/user' + user + '.csv')
+features['User'] = user
+features.to_csv('processed-feature-data/user' + user + '.csv')
